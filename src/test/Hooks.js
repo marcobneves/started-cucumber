402b13069950c8e5c4a9967955a8b21fdd0c9e93
Marco Barroso <marcobneves@gmail.com>
2019-10-02 13:43:10 -0400

    Created Hooks method

---
+++ src/test/Hooks.js
@@ -0,0 +1,13 @@
+
+import puppeteer from 'puppeteer';
+
+export const Hooks = async () => {
+    let page;
+    const browser = await puppeteer.launch({
+        headless: false,
+        executablePath: '/usr/bin/google-chrome'
+    });
+    return page = await browser.newPage();
+
+}
+
