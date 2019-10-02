95347a499beba611fa955617eb003a8b46f07fca
Leomar da Silva Barbosa <leomar.barbosa@kaiostech.com>
2019-10-02 17:30:58 -0400

    update features

--- src/test/Hooks.js
+++ src/test/Hooks.js
@@ -1,18 +1,6 @@
 
 import puppeteer from 'puppeteer';
 
-<<<<<<< HEAD
-export const Hooks = async() => {
-        let page;
-        const browser = await puppeteer.launch({
-            headless: false,
-            executablePath: '/usr/bin/google-chrome'
-        });
-        return page = await browser.newPage();
-
-}
-
-=======
 export const Hooks = async () => {
     const browser = await puppeteer.launch({
         headless: false,
@@ -24,4 +12,3 @@ export const Hooks = async () => {
 
 
 
->>>>>>> f9534bf5e332e7199afbd569529dd8be06fd3b0a
--- src/test/login_steps.js
+++ src/test/login_steps.js
@@ -1,18 +1,22 @@
+import { Before, Given, When, Then } from 'cucumber';
+import {Hooks} from './Hooks';
 
-         Given('that view url {string}', function (string) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-       
+let page;
+
+Before(async () => {
+    page = await Hooks();
+});
+
+Given('that view url {string}', async (url)=> {
+    page.goto(url);
+});
 
 
-         When('insert email {string} and password {string}', function (string, string2) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
- 
+When('insert email {string} and password {string}', function (email, senha) {
 
-         Then('view my authentication with success', function () {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+});
+
+
+Then('view my authentication with success', function () {
+  
+});
